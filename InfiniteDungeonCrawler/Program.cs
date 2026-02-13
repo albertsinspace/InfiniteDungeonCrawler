062878054f726ab06945f222f7461cb46b8ff2e7
alber <albert.cubeman@gmail.com>
2026-02-13 23:06:11 +0000

    Added main game logic

--- InfiniteDungeonCrawler/Program.cs
+++ InfiniteDungeonCrawler/Program.cs
@@ -1,2 +1,43 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿bool isMainGameLoopActive = true;
+
+while (isMainGameLoopActive)
+{
+    Console.Clear();
+    Console.WriteLine("Enter the TEXTGEON");
+    Console.WriteLine();
+    Console.WriteLine("1. Play");
+    Console.WriteLine("2. How to play");
+    Console.WriteLine("3. Quit");
+    Console.WriteLine();
+    Console.WriteLine("Enter your choice: ");
+
+    string userMenuChoice = Console.ReadLine();
+
+    if (userMenuChoice == "1")
+    {
+        Console.Clear();
+        Console.WriteLine("Let's play");
+        Console.ReadLine();
+    }
+
+    else if (userMenuChoice == "2")
+    {
+        Console.Clear();
+        Console.WriteLine("How to play");
+        Console.ReadLine();
+    }
+
+    else if (userMenuChoice == "3")
+    {
+        Console.Clear();
+        Console.WriteLine("Goodbye");
+        break;
+    }
+
+    else
+    {
+        Console.Clear();
+        Console.WriteLine("Incorrect input, please choose from the menu");
+        Console.ReadLine();
+    }
+}
