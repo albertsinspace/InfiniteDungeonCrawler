30bbb05ef11ce4aaee07984b9c4ababb9dc7b47c
alber <albert.cubeman@gmail.com>
2026-02-16 20:37:09 +0000

    Started with main game logic and changed the front page art

--- InfiniteDungeonCrawler/Program.cs
+++ InfiniteDungeonCrawler/Program.cs
@@ -1,24 +1,60 @@
-﻿bool isMainGameLoopActive = true;
+﻿//ascii art generator from here: https://patorjk.com/software/taag/#p=display&f=Graffiti&t=TEXTGEON&x=none&v=4&h=4&w=80&we=false
+
+bool isGameMenuActive = true;
 bool isGameLoopActive = true;
 
-while (isMainGameLoopActive)
+List<string> startingText = new List<string> {"In a land as far as it is wide...", "As the sun rises to mark the age of a new dawn...", "With great bravery comes great sacrifice..."};
+List<string> userInventory = new List<string> {"Wooden Sword", "Minor Healing Potion"};
+
+int playerGold = 0;
+
+Random rnd = new Random();
+int ranNum = rnd.Next(0, startingText.Count());
+
+string ranStartingText = startingText[ranNum];
+
+string playerName = "";
+
+while (isGameMenuActive)
 {
     Console.Clear();
-    Console.WriteLine("Enter the TEXTGEON");
+    Console.WriteLine(@"__________________________  _________________________________________    _______   
+\__    ___/\_   _____/\   \/  /\__    ___/  _____/\_   _____/\_____  \   \      \  
+  |    |    |    __)_  \     /   |    | /   \  ___ |    __)_  /   |   \  /   |   \ 
+  |    |    |        \ /     \   |    | \    \_\  \|        \/    |    \/    |    \
+  |____|   /_______  //___/\  \  |____|  \______  /_______  /\_______  /\____|__  /
+                   \/       \_/                 \/        \/         \/         \/ ");
     Console.WriteLine();
     Console.WriteLine("1. Play");
     Console.WriteLine("2. How to play");
     Console.WriteLine("3. Quit");
     Console.WriteLine();
-    Console.WriteLine("Enter your choice... ");
+    Console.Write("Enter your choice... ");
 
     var userMenuChoice = Console.ReadLine();
 
     if (userMenuChoice == "1")
     {
         Console.Clear();
-        Console.WriteLine("Let's play");
+        Console.WriteLine(ranStartingText);
         Console.ReadLine();
+        Console.WriteLine("Lives a warrior daring to enter the dungeons...");
+        Console.ReadLine();
+        Console.WriteLine("May this brave warrior go far and may the Gods show mercy upon their soul.");
+        Console.ReadLine();
+        Console.Clear();
+        Console.Write("What is your name, brave warrior... ");
+        playerName = Console.ReadLine();
+        Console.Clear();
+        Console.WriteLine($"Welcome, {playerName}.");
+        Console.ReadLine();
+        Console.Write("Are you ready?... (Press any key to continue) ");
+        Console.ReadLine();
+
+        while (isGameLoopActive)
+        {
+            Console.Clear();
+        }
     }
 
     else if (userMenuChoice == "2")
@@ -33,14 +69,14 @@ while (isMainGameLoopActive)
     else if (userMenuChoice == "3")
     {
         Console.Clear();
-        Console.WriteLine("Goodbye");
-        isMainGameLoopActive = false;
+        Console.WriteLine("Goodbye, brave warrior...");
+        isGameMenuActive = false;
     }
 
     else
     {
         Console.Clear();
-        Console.WriteLine("Incorrect input, please choose from the menu");
+        Console.WriteLine("Incorrect input, please choose from the menu provided");
         Console.ReadLine();
     }
 }
